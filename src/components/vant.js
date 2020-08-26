fb34e7969e8e51203e6d5a5349b8358f955d5eba
huohuo <18618110533@163.com>
2020-08-26 14:43:40 +0800

    新增demo页

--- src/components/vant.js
+++ src/components/vant.js
@@ -1,9 +1,9 @@
-import { Row, Col, Botton, Toast } from "vant";
+import { Row, Col, Button, Toast } from "vant";
 const vant = {
   install: function(Vue) {
     Vue.component("van-row", Row);
     Vue.component("van-col", Col);
-    Vue.component("van-button", Botton);
+    Vue.component("van-button", Button);
     Vue.component("van-toast", Toast);
   }
 };
